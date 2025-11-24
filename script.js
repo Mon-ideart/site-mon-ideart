b1a1b9ee29ca658cc7a34125c40cb7d3afd82bb7
Mon-ideart <juliafilou@gmail.com>
2025-11-24 15:25:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 
-// Gestion du formulaire de contact
+// Gestion du formulaire
 document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Empêche l'envoi classique du formulaire
-
-    const name = event.target.name.value;
-    const email = event.target.email.value;
-    const message = event.target.message.value;
-
-    if(name && email && message) {
-        alert('Message envoyé ! Merci, ' + name);
-        event.target.reset(); // Réinitialiser le formulaire après envoi
-    } else {
-        alert('Tous les champs doivent être remplis.');
-    }
+  event.preventDefault();
+  const name = event.target.name.value;
+  const email = event.target.email.value;
+  const message = event.target.message.value;
+  if (name && email && message) {
+    alert('Message envoyé ! Merci, ' + name);
+    event.target.reset();
+  } else {
+    alert('Tous les champs doivent être remplis.');
+  }
 });
+
+// Gestion du menu burger
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+
+                    
